e0ae8c5c69a73ba2a0fecb79a6ab47c32500fc21
Fabiano Florentino <fabianoflorentino@outlook.com>
2020-05-01 11:22:13 -0300

    removendo recurso não utilizado

--- route_tables_private.tf
+++ route_tables_private.tf
@@ -13,8 +13,8 @@ resource "aws_route_table" "natgw_private" {
 }
 
 
-resource "aws_route_table_association" "natgw_private" {
-  count          = var.az_count
-  subnet_id      = element(aws_subnet.private.*.id, count.index)
-  route_table_id = element(aws_route_table.natgw_private.*.id, count.index)
-}
+# resource "aws_route_table_association" "natgw_private_association" {
+#   count          = var.az_count
+#   subnet_id      = element(aws_subnet.private.*.id, count.index)
+#   route_table_id = element(aws_route_table.natgw_private.*.id, count.index)
+# }
